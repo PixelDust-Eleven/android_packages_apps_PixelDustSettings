5889382b3e48824f3dc519a64af3a3e2ce510b6c
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 22:47:35 +0530

    Notifications Settings: Bring back Gaming mode [3/3]
    AOSiP: Improve nearly all strings
    
    Havoc-OS:
        - Remove hardware key entries
        - Use MasterSettingSwitch
    
    Change-Id: I04e0c140499cd8d190bc9ffb962cd33797563357
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/NotificationsSettings.java
+++ src/com/pixeldust/settings/fragments/NotificationsSettings.java
@@ -41,14 +41,21 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.pixeldust.settings.preferences.SystemSettingMasterSwitchPreference;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @SearchIndexable
-public class NotificationsSettings extends SettingsPreferenceFragment {
+public class NotificationsSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String GAMING_MODE_ENABLED = "gaming_mode_enabled";
 
     private Preference mChargingLeds;
+    private Preference mAlertSlider;
+    private SystemSettingMasterSwitchPreference mGamingMode;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -61,6 +68,28 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
         if (!getResources().getBoolean(R.bool.has_notification_light)) {
             prefScreen.removePreference(LightOptions);
         }
+
+        mGamingMode = (SystemSettingMasterSwitchPreference) findPreference(GAMING_MODE_ENABLED);
+        boolean gameEnabled = Settings.System.getInt(
+        getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.GAMING_MODE_ENABLED, 0) == 1;
+        updateGameModeEnabledUpdatePrefs(gameEnabled);
+        mGamingMode.setOnPreferenceChangeListener(this);
+    }
+
+    private void updateGameModeEnabledUpdatePrefs(boolean gameEnabled) {
+        mGamingMode.setChecked(gameEnabled);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGamingMode) {
+            boolean gameEnabled = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.GAMING_MODE_ENABLED, gameEnabled ? 1 : 0);
+            updateGameModeEnabledUpdatePrefs(gameEnabled);
+            return true;
+        }
+        return false;
     }
 
     @Override
