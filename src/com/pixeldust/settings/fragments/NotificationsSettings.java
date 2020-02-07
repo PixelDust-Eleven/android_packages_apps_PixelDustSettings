efb3faceb4972d68762214dba0fa015cc480364e
spezi77 <spezi7713@gmx.net>
2020-02-07 12:13:54 +0100

    NotificationsSettings: Allow to remove the LED options for unsupported devices.
    Usage: Add an overlay to your device tree:
    
        <!-- Whether to display notification light category -->
        <bool name="has_notification_light">false</bool>
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/NotificationsSettings.java
+++ src/com/pixeldust/settings/fragments/NotificationsSettings.java
@@ -57,11 +57,9 @@ public class NotificationsSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.pixeldust_settings_notifications);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        mChargingLeds = (Preference) findPreference("charging_light");
-        if (mChargingLeds != null
-                && !getResources().getBoolean(
-                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
-            prefScreen.removePreference(mChargingLeds);
+        Preference LightOptions = findPreference("light_options");
+        if (!getResources().getBoolean(R.bool.has_notification_light)) {
+            prefScreen.removePreference(LightOptions);
         }
     }
 
