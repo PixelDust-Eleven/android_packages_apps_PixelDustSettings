13332840b71c1f14569376af2237da0c8f39952c
spezi77 <spezi7713@gmx.net>
2020-10-10 08:53:26 +0200

    PixelDustSettings: Add all fragments to the search index.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/LockScreenSettings.java
+++ src/com/pixeldust/settings/fragments/LockScreenSettings.java
@@ -15,13 +15,35 @@
  */
 package com.pixeldust.settings.fragments;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.provider.SearchIndexableResource;
+import android.widget.Toast;
+
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.os.Bundle;
-import com.android.settings.R;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import com.android.settings.SettingsPreferenceFragment;
-
+@SearchIndexable
 public class LockScreenSettings extends SettingsPreferenceFragment {
 
     @Override
@@ -35,4 +57,22 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.PIXELDUST;
     }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(
+                        Context context, boolean enabled) {
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.pixeldust_settings_lockscreen;
+                    return Arrays.asList(sir);
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
 }
